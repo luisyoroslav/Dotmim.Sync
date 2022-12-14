b7d0c23377b4ca8ea3c4dc26a95a148bdd5c5d47
René Mogro <rene110882@gmail.com>
2022-12-13 21:30:50 -0300

    .

--- Projects/Dotmim.Sync.PostgreSql/NpgsqlSyncProvider.cs
+++ Projects/Dotmim.Sync.PostgreSql/NpgsqlSyncProvider.cs
@@ -13,22 +13,33 @@ namespace Dotmim.Sync.Postgres
     public class NpgsqlSyncProvider : CoreProvider
     {
         private DbMetadata dbMetadata;
-        static string providerType;
+        private NpgsqlConnectionStringBuilder builder;
+
+
         public NpgsqlSyncProvider() : base()
         { }
 
-        public NpgsqlSyncProvider(string connectionString) : base() => this.ConnectionString = connectionString;
+        public NpgsqlSyncProvider(string connectionString) : base()
+        {
+            this.ConnectionString = connectionString;
+
+            if (!string.IsNullOrEmpty(this.ConnectionString))
+            {
+                this.builder = new NpgsqlConnectionStringBuilder(this.ConnectionString);
+            }
+        }
 
         public NpgsqlSyncProvider(NpgsqlConnectionStringBuilder builder) : base()
         {
-            if (String.IsNullOrEmpty(builder.ConnectionString))
+            if (string.IsNullOrEmpty(builder.ConnectionString))
                 throw new Exception("You have to provide parameters to the Sql builder to be able to construct a valid connection string.");
 
+            this.builder = builder;
             this.ConnectionString = builder.ConnectionString;
         }
 
-        public override string ProviderTypeName => ProviderType;
-
+        static string providerType;
+        public override string GetProviderTypeName() => ProviderType;
         public static string ProviderType
         {
             get
@@ -44,30 +55,37 @@ namespace Dotmim.Sync.Postgres
 
         }
 
-
-
-        /// <summary>
-        /// Gets or sets the Metadata object which parse Sql server types
-        /// </summary>
-        public override DbMetadata Metadata
+        static string shortProviderType;
+        public override string GetShortProviderTypeName() => ShortProviderType;
+        public static string ShortProviderType
         {
             get
             {
-                if (dbMetadata == null)
-                    dbMetadata = new NpgsqlDbMetadata();
+                if (!string.IsNullOrEmpty(shortProviderType))
+                    return shortProviderType;
 
-                return dbMetadata;
-            }
-            set
-            {
-                dbMetadata = value;
+                var type = typeof(NpgsqlSyncProvider);
+                shortProviderType = type.Name;
 
+                return shortProviderType;
             }
         }
+        public override string GetDatabaseName()
+        {
+            if (builder != null && !String.IsNullOrEmpty(builder.Database))
+                return builder.Database;
 
-        /// <summary>
-        /// Gets a chance to make a retry connection
-        /// </summary>
+            return string.Empty;
+        }
+
+        public override DbMetadata GetMetadata()
+        {
+            if (this.dbMetadata == null)
+                this.dbMetadata = new NpgsqlDbMetadata();
+
+            return this.dbMetadata;
+        }
+        
         public override bool ShouldRetryOn(Exception exception)
         {
             if (exception is NpgsqlException)
@@ -75,6 +93,8 @@ namespace Dotmim.Sync.Postgres
 
             return true;
         }
+
+        public override (ParserName tableName, ParserName trackingName) GetParsers(SyncTable tableDescription, SyncSetup setup = null) => throw new NotImplementedException();
 
         public override void EnsureSyncException(SyncException syncException)
         {
@@ -89,38 +109,34 @@ namespace Dotmim.Sync.Postgres
             // Can add more info from SqlException
             var sqlException = syncException.InnerException as NpgsqlException;
 
-            if (sqlException == null)
-                return;
+            if (sqlException == null) return;
 
             syncException.Number = sqlException.ErrorCode;
-
-            return;
         }
 
-        /// <summary>
-        /// Sql server support bulk operations through Table Value parameter
-        /// </summary>
-        public override bool SupportBulkOperations => true;
-
-        /// <summary>
-        /// Sql Server supports to be a server side provider
-        /// </summary>
         public override bool CanBeServerProvider => true;
 
         public override DbConnection CreateConnection() => new NpgsqlConnection(this.ConnectionString);
-        public override DbScopeBuilder GetScopeBuilder() => new NpgsqlScopeBuilder();
-        public override DbTableBuilder GetTableBuilder(SyncTable tableDescription, SyncSetup setup)
+        public override DbScopeBuilder GetScopeBuilder(string scopeInfoTableName) => new NpgsqlScopeBuilder(scopeInfoTableName);
+        public override DbTableBuilder GetTableBuilder(SyncTable tableDescription, ParserName tableName, ParserName trackingTableName, SyncSetup setup, string scopeName)
         {
-            var tableBuilder = new NpgsqlTableBuilder(tableDescription, setup)
-            {
-                UseBulkProcedures = this.SupportBulkOperations,
-                UseChangeTracking = this.UseChangeTracking,
-                Filter = tableDescription.GetFilter()
-            };
+            //var tableBuilder = new NpgsqlTableBuilder(tableDescription, setup)
+            //{
+            //    UseBulkProcedures = this.SupportBulkOperations,
+            //    UseChangeTracking = this.UseChangeTracking,
+            //    Filter = tableDescription.GetFilter()
+            //};
 
-            return tableBuilder;
+            //return tableBuilder;
+            return null; // todo
         }
-        public override DbTableManagerFactory GetTableManagerFactory(string tableName, string schemaName) => new NpgsqlManager(tableName, schemaName);
+
+        public override DbSyncAdapter GetSyncAdapter(SyncTable tableDescription, ParserName tableName, ParserName trackingTableName, SyncSetup setup, string scopeName)
+        {
+            //return new NpgsqlSyncAdapter(tableName, setup);
+            return null; // todo
+        }
+
         public override DbBuilder GetDatabaseBuilder() => new NpgsqlBuilder();
 
     }
